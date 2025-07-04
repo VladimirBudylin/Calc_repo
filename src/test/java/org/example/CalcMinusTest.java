5fbf575020eb50204c920aef3c6df89d5532e344
Vladimir Budylin <133401681+VladimirBudylin@users.noreply.github.com>
2025-07-05 00:47:25 +0700

    Add subb tests

--- src/test/java/org/example/CalcMinusTest.java
+++ src/test/java/org/example/CalcMinusTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,4 +23,17 @@ class CalcMinusTest {
             int result = calculator.CalcMinus (10,9);
             Assertions.assertEquals(1, result, "Ответ неверный");
         }
+
+
+
+    @ParameterizedTest(name = "#{index} Вычитая {0} и {1} ожидаем {2}")
+    @DisplayName(value = "Результат вычитания")
+    @CsvSource ({ "10, 1,9", "100, 99, 1", "0, 0, 0"})
+    void subb2(int c, int d , int subbResult) {
+        CalcMinus calculator = new CalcMinus();
+        int result = calculator.CalcMinus(c,d);
+        Assertions.assertEquals(subbResult,result, "Ответ неверный");
     }
+}
+
+
