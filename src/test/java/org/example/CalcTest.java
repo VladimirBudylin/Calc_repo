6705dfd1cfd0c830aa7f9b6ac825b1bdcdc100ed
Vladimir Budylin <133401681+VladimirBudylin@users.noreply.github.com>
2025-07-05 00:54:25 +0700

    Add subb tests, add test result

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -74,4 +74,26 @@ class CalcTest {
         int result = calculator.CalcPlus(a,b);
         Assertions.assertEquals(expectedResult, result,"Ответ неверный");
     }
-}
+
+        @Test
+        @RepeatedTest(10)
+        @DisplayName(value = "Проверка вычитания")
+
+        void subb1 () {
+
+            CalcMinus calculator = new CalcMinus();
+            int result = calculator.CalcMinus (10,9);
+            Assertions.assertEquals(1, result, "Ответ неверный");
+        }
+
+
+
+        @ParameterizedTest(name = "#{index} Вычитая {0} и {1} ожидаем {2}")
+        @DisplayName(value = "Результат вычитания")
+        @CsvSource ({ "10, 1,9", "100, 99, 1", "0, 0, 0"})
+        void subb2(int c, int d , int subbResult) {
+            CalcMinus calculator = new CalcMinus();
+            int result = calculator.CalcMinus(c,d);
+            Assertions.assertEquals(subbResult,result, "Ответ неверный");
+        }
+    }
